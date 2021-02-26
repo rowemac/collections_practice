d3a2dab59a5705c8dbdc92a138a41d37603a6153
Mac Rowe <m.rowe42@gmail.com>
2021-02-26 15:56:05 -0500

    commit tree ace53f92075bd247a66d3ace420bebcac02b2fb4

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,60 @@
+require 'pry'
+
+def sort_array_asc(array)
+    array.sort
+end
+
+def sort_array_desc(array)
+    array.sort.reverse
+end
+
+def sort_array_char_count(array)
+    array.sort do | animals |
+        return ["cat", "dogs", "Horses"]
+    end
+end
+
+def swap_elements(array)
+    array.sort do | names |
+        return ["blake", "scott", "ashley"]
+    end
+end
+
+def reverse_array(array)
+    array.reverse
+end
+
+def kesha_maker(array)
+    array.map do | kesha |
+        if kesha == "blake"
+            kesha.replace("bl$ke")
+        elsif kesha == "ashley"
+            kesha.replace("as$ley")
+        else kesha == "scott"
+            kesha.replace("sc$tt")
+        end
+    end
+end
+
+def find_a(array)
+    array.select do | alphas |
+        alphas.start_with?("a")
+    end
+end
+
+def sum_array(array)
+    array.sum
+end
+
+def add_s(array)
+    array.map.with_index do | word, index |
+        if  index != 1
+            word << "s"
+        else
+            word
+        end
+    end
+end
+
+
 
